4fa860adcfa5d868b46a448e5935f669f5101bb6
GowthamS05 <raogowtham@gmail.com>
2021-05-27 16:56:05 +0530

    My portfolio

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Portfolio
+# Project Title
+
 My Personal Portfolio
+
+## Description
+
+Describes about my career information.
+
+## Getting Started
+
+### Dependencies
+
+- Designed using HTML,CSS,Javascript.
+- Google fonts used
+- Library used for scrolling and animation.
+
+### Installing
+
+- Download the project and Click on index.html to view the project
+
+## Authors
+
+Gowtham S
+
+## Acknowledgments
+
+- youtube
+  Special thanks to
+  (BedimCode)
---
+++ assets/js/main.js
@@ -0,0 +1,168 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+/*===== MENU HIDDEN =====*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*================= CLOSE MENU ON CLICK ================*/
+const navLink = document.querySelectorAll(".nav_link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+    if (skillsContent[i].children.length > 0) {
+      skillsContent[i].children[1].className = "skills_list grid";
+    }
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+    if (this.parentNode.children.length > 0) {
+      this.parentNode.children[1].className =
+        "skills_list grid animate__animated animate__fadeIn";
+    }
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification_active");
+    });
+    target.classList.add("qualification_active");
+    tab.forEach((tab) => {
+      tab.classList.remove("qualification_active");
+    });
+    tab.classList.add("qualification_active");
+  });
+});
+
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services_modal"),
+  modalBtns = document.querySelectorAll(".services_button"),
+  modalCloses = document.querySelectorAll(".services_modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+modalCloses.forEach((close) => {
+  close.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper(".portfolio_container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+/*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial_container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true
+  },
+  breakpoints: {
+    620: {
+      slidesPerView: 2
+    }
+  }
+});
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const lightTheme = "light-theme";
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(lightTheme);
+  if (document.body.classList.contains(lightTheme)) {
+    themeButton.classList.remove("uil-sun");
+    themeButton.classList.add("uil-moon");
+  } else {
+    themeButton.classList.remove("uil-moon");
+    themeButton.classList.add("uil-sun");
+  }
+});
